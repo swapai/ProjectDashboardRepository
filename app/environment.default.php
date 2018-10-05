53f5cc31e2698b3c710a983907d61bea797f3875
Swati Pai <swapai@atlassian.com>
2018-10-05 12:09:08 -0400

    Environment files

---
+++ app/environment.default.php
@@ -0,0 +1,4 @@
+<?php
+const DB_USER = 'username';
+const DB_PW   = 'hello';
+const DB_SERVER = 'mysql:host=localhost;dbname=dashboard_db';
---
+++ app/environment.php
@@ -0,0 +1,5 @@
+<?php
+
+const DB_USER = 'root';
+const DB_PW   = 'kteam12';
+const DB_SERVER = 'mysql:host=localhost;dbname=Final_Project';
