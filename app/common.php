19f2591e70c11ce97b4e0eafad3b001f1628b065
Swati Pai <swapai@atlassian.com>
2018-10-05 12:35:24 -0400

    including Client.php in common.php

--- app/common.php
+++ app/common.php
@@ -1,5 +1,14 @@
 <?php
 
-const DB_USER = 'root';
-const DB_PW   = 'kteam12';
-const DB_SERVER = 'mysql:host=localhost;dbname=Final_Project';
+// Change the working directory to this file.
+chdir(__DIR__);
+set_include_path (__DIR__);
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' &&
+stripos($_SERVER['CONTENT_TYPE'],'application/json') !== false){
+  $_POST = json_decode(file_get_contents('php://input'), true);
+}
+
+require 'environment.php';
+/** MODELS **/
+require 'models/Client.php';
