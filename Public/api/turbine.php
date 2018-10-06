f237d7a9322852f6f4ccba760ed5c5ddac8d8fab
Jennie Chen <chen417@iu.edu>
2018-10-06 14:55:08 -0400

    add getTurbineByTurbineId

--- Public/api/turbine.php
+++ Public/api/turbine.php
@@ -16,10 +16,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 // 1. Go to the database and get all work associated with the $taskId
 $turbineArr = Turbine::getTurbine();
-
+$turbineItem = Turbine::getTurbineByTurbineId();
 // 2. Convert to JSON
 $json = json_encode($turbineArr, JSON_PRETTY_PRINT);
-
+$json = json_encode($turbineItem, JSON_PRETTY_PRINT);
 // 3. Print
 header('Content-Type: application/json');
 echo $json;
--- app/models/Turbine.php
+++ app/models/Turbine.php
@@ -56,5 +56,25 @@ class Turbine
 
           $this->turbineId = $db->lastInsertId();
         }
+        public static function getTurbineByTurbineId(int $turbineId) {
+          // 1. Connect to the database
+          $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+          // 2. Prepare the query
+          $sql = 'SELECT * FROM turbine WHERE turbineId = ?';
+          $statement = $db->prepare($sql);
+          // 3. Run the query
+          $success = $statement->execute(
+              [$turbineId]
+          );
+          // 4. Handle the results
+          $arr = [];
+          while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            // 4.a. For each row, make a new work object
+            $turbineItem =  new Turbine($row);
+            array_push($arr, $turbineItem);
+          }
+            return $arr;
+        }
+
 
   }
