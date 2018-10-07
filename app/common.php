1ed61801bd972179ad5bf7ff069afff4ded3c5f6
Jennie Chen <chen417@iu.edu>
2018-10-07 09:30:40 -0400

    add turbinedeployed to common

--- app/common.php
+++ app/common.php
@@ -13,6 +13,7 @@ require 'environment.php';
 /** MODELS **/
 require 'models/Client.php';
 require 'models/Turbine.php';
+require 'models/TurbineDeployed.php';
 require 'models/Sensor.php';
 require 'models/SensorDeployed.php';
 require 'models/SensorTimeSeries.php';
