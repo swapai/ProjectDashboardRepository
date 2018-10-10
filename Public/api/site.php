adb3a9f0005d7a21b24346f198a27cfca74532cb
Ellie Ramey <elramey@iu.edu>
2018-10-10 09:38:15 -0400

    edited site php files

---
+++ Public/api/site.php
@@ -0,0 +1,21 @@
+<?php
+require '../../app/common.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  require 'sitePost.php';
+  exit;
+}
+// $clientId = intval($_GET['clientId'] ?? 0);
+// if ($clientId >= 1) {
+//   $clientArr = Client::getClientByClientId($clientId);
+//   $json = json_encode($clientArr, JSON_PRETTY_PRINT);
+//   // 3. Print
+//   header('Content-Type: application/json');
+//   echo $json;
+//   exit;
+// }
+$sites = Site::fetchAll();
+$json = json_encode($sites, JSON_PRETTY_PRINT);
+// 3. Print
+header('Content-Type: application/json');
+echo $json;
---
+++ Public/api/sitePost.php
@@ -0,0 +1,7 @@
+<?php
+
+$site = new Site($_POST);
+
+$site->create();
+
+echo json_encode($site);
