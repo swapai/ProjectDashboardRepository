91f6167359c1d3692d25f939382835ef4bc25edd
Jennie Chen <chen417@iu.edu>
2018-10-18 07:15:22 -0400

    add click

--- Public/js/index.app.js
+++ Public/js/index.app.js
@@ -3,8 +3,20 @@ var clientsApp = new Vue({
   data: {
   clientList: []
 },
-
+methods: {
+  gotoSite(cid) {
+    window.location = 'task.html?clientId=' + cid;
+  }
+},
   created (){
+    const url = new URL(window.location.href);
+    const clientId = url.searchParams.get('clientId') || 0;
+
+    if (!clientId) {
+      console.error('client Id not defined in URL parameters.')
+    }
+
+
     fetch('api/client.php')
       .then( response => response.json() )
       .then( json => {clientsApp.clientList = json} )
