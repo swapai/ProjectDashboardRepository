b309746ccbbededef2e72d8deefbeb78b6b1cab0
Jennie Chen <chen417@iu.edu>
2018-10-18 07:47:34 -0400

    working stage

--- Public/js/sites.app.js
+++ Public/js/sites.app.js
@@ -1,57 +1,25 @@
-//copied from comment assignment, still need updates
-
-var commentsApp = new Vue({
-  el: '#commentMain',
+var sitesApp = new Vue({
+  el: '#siteMain',
   data: {
+  siteList: []
+},
+// methods: {
+//   gotoSite(cid) {
+//     window.location = 'site.html?clientId=' + cid;
+//   }
+// },
+  created (){
+    const url = new URL(window.location.href);
+        const clientId = url.searchParams.get('clientId');
+        console.log('Clinet: '+ clientId);
+        this.clientList.clientId = clientId;
 
-    commentForm: { },   // populated by this.getEmptyWorkForm()
-    commentList: []
-  },
-
-  methods: {
-    handleCommentForm(e) {
-
-      const s = JSON.stringify(this.commentForm);
-
-            console.log(s);
-
-      fetch('api/comment.php', {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        headers: {
-            "Content-Type": "application/json; charset=utf-8"
-        },
-        body: s // body data type must match "Content-Type" header
-      })
+    fetch('api/site.php')
       .then( response => response.json() )
-      .then( json => {this.commentList.push(json)})
+      .then( json => {sitesApp.siteList = json} )
       .catch( err => {
-        console.error('COMMENT POST ERROR:');
+        console.error('SITE FETCH ERROR:');
         console.error(err);
       })
-
-      // Reset workForm
-      this.commentForm = this.getEmptyCommentForm();
-    },
-
-    getEmptyCommentForm () {
-      return {
-        comment: null //this.commentdata.comment
-      }
-    }
-  },
-  created () {
-    // Populate workForm with default values
-    this.commentForm = this.getEmptyCommentForm();
-
-    // TODO: Fetch task-specific data
-    // fetch('api/task?id=4')
-    fetch('api/comment.php')
-    .then( response => response.json() )
-    .then( json => {commentsApp.commentList = json} )
-    .catch( err => {
-      console.error('COMMENT FETCH ERROR:');
-      console.error(err);
-    })
   }
-
 })
