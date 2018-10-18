e0d59cf0668d44f19e5695daab70046c63dd8ece
Jennie Chen <chen417@iu.edu>
2018-10-18 06:39:16 -0400

    fix fetch error

--- Public/js/index.app.js
+++ Public/js/index.app.js
@@ -1,20 +1,16 @@
-//need edit
-
-var indexApp = new Vue({
-  el: '#index',
+var clientsApp = new Vue({
+  el: '#clientMain',
   data: {
-    clientList: [ ]
-  },
+  clientList: []
+},
 
-
-  created () {
-
+  created (){
     fetch('api/client.php')
-    .then( response => response.json() )
-    .then( json => {indexApp.clientList = json} )
-    .catch( err => {
-    console.error('CLIENT FETCH ERROR:');
-    console.error(err);
-    })
+      .then( response => response.json() )
+      .then( json => {clientsApp.clientList = json} )
+      .catch( err => {
+        console.error('CLIENT FETCH ERROR:');
+        console.error(err);
+      })
   }
 })
