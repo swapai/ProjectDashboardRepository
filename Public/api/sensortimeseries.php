4bfc45047fa99350f55271fbdb049084b7b80f1c
Swati Pai <swapai@atlassian.com>
2018-10-21 12:39:10 -0400

    Time series data fetch test code

--- Public/api/sensortimeseries.php
+++ Public/api/sensortimeseries.php
@@ -5,16 +5,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require 'sensortimeseriesPost.php';
   exit;
 }
-// $clientId = intval($_GET['clientId'] ?? 0);
-// if ($clientId >= 1) {
-//   $clientArr = Client::getClientByClientId($clientId);
-//   $json = json_encode($clientArr, JSON_PRETTY_PRINT);
-//   // 3. Print
-//   header('Content-Type: application/json');
-//   echo $json;
-//   exit;
-// }
-$sensortimeseries = SensorTimeSeries::fetchAll();
+$sensorDeployedId = intval($_GET['sensorDeployedId'] ?? 0);
+if ($sensorDeployedId < 1) {
+  throw new Exception('Invalid Sensor Deployed ID');
+}
+$sensortimeseries = SensorTimeSeries::getSensorTimeSeriesBySensorDeployedId($sensorDeployedId);
 $json = json_encode($sensortimeseries, JSON_PRETTY_PRINT);
 // 3. Print
 header('Content-Type: application/json');
---
+++ Public/js/kpi.app.js
@@ -0,0 +1,34 @@
+var kpiApp = new Vue({
+  el: '#kpiMain',
+  data: {
+    kpi: {
+      sensorDeployedId: 0,
+      dataCollectiveDate: '',
+      output : '',
+      heatRate : '',
+      compressorEfficiency : '',
+      availability: '',
+      reliability: '',
+      firedHours: '',
+      trips: '',
+      starts: ''
+    },
+  kpiList: []
+},
+
+  created (){
+
+    const url = new URL(window.location.href);
+    const sensorDeployedId = url.searchParams.get('sensorDeployedId');
+    console.log('SensorDeployed Id: '+ sensorDeployedId);
+    this.kpi.sensorDeployedId = sensorDeployedId;
+
+    fetch('api/$sensortimeseries.php?sensorDeployedId='+sensorDeployedId)
+    .then( response => response.json() )
+    .then( json => {kpiApp.kpiList = json} )
+    .catch( err => {
+      console.error('SITE FETCH ERROR:');
+      console.error(err);
+    })
+  }
+})
