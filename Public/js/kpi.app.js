765e795564f0150b9eda61fd62fc9728bcc4c9da
Swati Pai <swapai@atlassian.com>
2018-10-21 14:02:41 -0400

    Output graph attempt

--- Public/js/kpi.app.js
+++ Public/js/kpi.app.js
@@ -14,8 +14,59 @@ var kpiApp = new Vue({
       starts: ''
     },
   kpiList: []
-},
+},methods:{
+  buildOutputChart(){
+      Highcharts.chart('outputChart', {
+            title: {
+                text: 'Cumulative Sensor Output'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Power'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
 
+            series: [{
+                type: 'area',
+                name: 'Hours (Running Total)',
+                // Data needs [ [date, num], [date2, num2 ], ... ]
+                data: this.kpiList.map( item => [item.dataCollectiveDate, item.output] )
+            }]
+        });
+  }
+},
   created (){
 
     const url = new URL(window.location.href);
