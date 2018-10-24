7515722627662a05e5f4a474365ddd82fbe47c26
Jennie Chen <chen417@iu.edu>
2018-10-24 11:19:16 -0400

    client html

--- Public/api/notes.php
+++ Public/api/notes.php
@@ -6,7 +6,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   exit;
 }
 $clientId = intval($_GET['clientId'] ?? 0);
-
+if ($clientId < 1) {
+  throw new Exception('Invalid Client ID');
+}
 
 
 $notes = Notes::getNotesByClientsId($clientId);
