6f43a060bb1fc62d56161fd6a17e382771c106ca
Jennie Chen <chen417@iu.edu>
2018-10-24 11:16:09 -0400

    debug

--- Public/api/notes.php
+++ Public/api/notes.php
@@ -5,15 +5,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require 'notesPost.php';
   exit;
 }
-// $clientId = intval($_GET['clientId'] ?? 0);
-// if ($clientId >= 1) {
-//   $clientArr = Client::getClientByClientId($clientId);
-//   $json = json_encode($clientArr, JSON_PRETTY_PRINT);
-//   // 3. Print
-//   header('Content-Type: application/json');
-//   echo $json;
-//   exit;
-// }
+$clientId = intval($_GET['clientId'] ?? 0);
+if ($clientId < 1) {
+  throw new Exception('Invalid Client ID');
+}
+
+
 $notes = Notes::getNotesByClientsId($clientId);
 $json = json_encode($notes, JSON_PRETTY_PRINT);
 // 3. Print
