fc4bea5baa5bacaf5b9286fb231859e252727c6c
Jennie Chen <chen417@iu.edu>
2018-10-26 17:12:57 -0400

    add heatrate chart

--- Public/js/kpi.app.js
+++ Public/js/kpi.app.js
@@ -21,6 +21,7 @@ var kpiApp = new Vue({
     .then( json => {
       kpiApp.kpiList = json;
       this.buildOutputChart();
+      this.buildHeatRateChart();
     } )
     .catch( err => {
       console.error('SITE FETCH ERROR:');
@@ -79,8 +80,123 @@ var kpiApp = new Vue({
                 data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.output)] )
             }]
         });
+  },
+  buildHeatRateChart(){
+    Highcharts.chart('heatRateChart', {
+
+    chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+
+    title: {
+        text: 'Sensor Heat Rate'
+    },
+
+    pane: {
+        startAngle: -150,
+        endAngle: 150,
+        background: [{
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#FFF'],
+                    [1, '#333']
+                ]
+            },
+            borderWidth: 0,
+            outerRadius: '109%'
+        }, {
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#333'],
+                    [1, '#FFF']
+                ]
+            },
+            borderWidth: 1,
+            outerRadius: '107%'
+        }, {
+            // default background
+        }, {
+            backgroundColor: '#DDD',
+            borderWidth: 0,
+            outerRadius: '105%',
+            innerRadius: '103%'
+        }]
+    },
+
+    // the value axis
+    yAxis: {
+        min: 9000,
+        max: 12000,
+
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 10,
+        minorTickPosition: 'inside',
+        minorTickColor: '#666',
+
+        tickPixelInterval: 30,
+        tickWidth: 2,
+        tickPosition: 'inside',
+        tickLength: 10,
+        tickColor: '#666',
+        labels: {
+            step: 2,
+            rotation: 'auto'
+        },
+        title: {
+            text: 'kWh'
+        },
+        plotBands: [{
+            from: 9000,
+            to: 10000,
+            color: '#55BF3B' // green
+        }, {
+            from: 10000,
+            to: 11000,
+            color: '#DDDF0D' // yellow
+        }, {
+            from: 11000,
+            to: 12000,
+            color: '#DF5353' // red
+        }]
+    },
+
+    series: [{
+        name: 'heatRate',
+        data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.heatRate)] ),
+        tooltip: {
+            valueSuffix: ' kWh'
+        }
+    }]
+
+},
+// Add some life
+function (chart) {
+    if (!chart.renderer.forExport) {
+        setInterval(function () {
+            var point = chart.series[0].points[0],
+                newVal,
+                inc = Math.round((Math.random() - 0.5) * 20);
+
+            newVal = point.y + inc;
+            if (newVal < 0 || newVal > 200) {
+                newVal = point.y - inc;
+            }
+
+            point.update(newVal);
+
+        }, 3000);
+    }
+});
   }
 },
+
   created (){
 
     const url = new URL(window.location.href);
