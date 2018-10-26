a47d61c9e3d12917641e0dc8aa95c503daab1590
Jennie Chen <chen417@iu.edu>
2018-10-26 17:52:05 -0400

    change chart type

--- Public/js/kpi.app.js
+++ Public/js/kpi.app.js
@@ -115,45 +115,25 @@ var kpiApp = new Vue({
 },
 buildCompressorEfficiencyChart(){
   Highcharts.chart('compressorEfficiencyChart', {
-
-    xAxis: {
-      categories: [
-          'Jan',
-          'Feb',
-          'Mar',
-          'Apr',
-          'May',
-          'Jun',
-          'Jul',
-          'Aug',
-          'Sep',
-          'Oct',
-          'Nov',
-          'Dec'
-      ],
-        crosshair: true
-    },
     yAxis: {
-        min: 0,
-        max: 100,
-        title: {
-            text: 'Efficiency (%)'
-        }
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
+            title: {
+                text: 'Compressor Efficiency (%)'
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                }
+            }
+        },
+
     series: [{
         type: 'column',
         name: 'Compressor Efficiency (%)',
