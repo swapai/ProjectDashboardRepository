53494dcd29e805a9068e64c6d89833d942f3092f
Swati Pai <swapai@atlassian.com>
2018-10-28 19:35:08 -0400

    code changes

--- Public/js/kpi.app.js
+++ Public/js/kpi.app.js
@@ -196,7 +196,7 @@ buildFiredHoursChart(){
                 },
 
                 series: [{
-                    data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.runningTotalFiredHours)] ),
+                    data: this.kpiList.map( item => [Date.parse(item.dataCollectiveDate), Number(item.firedHours)] ),
                     lineColor: Highcharts.getOptions().colors[1],
                     color: Highcharts.getOptions().colors[2],
                     fillOpacity: 0.5,
